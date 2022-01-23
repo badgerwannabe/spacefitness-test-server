0c081bf5a0cbedfbeda6ec43d494a2bf523593aa
badgerwannabe <badgerwannabe@gmail.com>
2022-01-23 14:55:53 +0100

    changed something in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 const typeDefs = require("./graphql/typeDefs")
-const resolvers = require('./graphql/resolvers')
+const resolvers = require('./graphql/resolvers/')
 
 const express = require("express");
 
